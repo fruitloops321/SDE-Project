89e0bcd37c81ba26b32d04de443fee8fdd4d1e98
Maman1018 <75409000+Maman1018@users.noreply.github.com>
2025-11-22 11:52:42 +0100

    fpSuccessful page added

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,10 @@ import Login from './pages/Login.jsx'
 import './index.css';
 import SignUp from './pages/SignUp.jsx'
 import ForgetPassword from './pages/ForgetPassword.jsx'
+import FPSuccessful from './pages/FPSucessful.jsx'
 
 function App() {
-  return <ForgetPassword />;
+  return <Dashboard />;
 }
 
 export default App;
